aca653d482b8f0a43c1a26ee9fb8d219b5af4933
Muhammad <mumer119131@gmail.com>
2021-07-19 18:26:54 +0500

    Added

---
+++ lock and change pass.py
@@ -0,0 +1,14 @@
+import re
+import subprocess
+import os
+
+#Gets User Name
+user_name = os.getlogin()
+#Changes user password
+process = subprocess.run(["net","user",user_name,"123456"])
+#Locks the window
+process = subprocess.run(["Rundll32.exe","user32.dll,LockWorkStation"])
+
+
+
+
